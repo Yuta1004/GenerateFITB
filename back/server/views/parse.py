720023200e2f6aef3d2f57234c02ab34f926b5fc
NakagamiYuta <guguru0014@gmail.com>
2021-02-06 16:07:41 +0900

    [fix] /parseでクエリストリングから対象文字列を受け取るようにした

--- back/server/views/parse.py
+++ back/server/views/parse.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from server.parser.parser import gen_word_class_table
 
@@ -9,7 +9,10 @@ route_parse = Blueprint(__name__, "parse")
 
 @route_parse.route("/parse")
 def parse():
-    target = "メロスは激怒した。必ず、かの邪智暴虐の王を除かなければならぬと決意した。"
+    if "target" not in request.args:
+        return jsonify({"table": []})
+
+    target = request.args.get("target")
     word_class_table, class_id_table = gen_word_class_table(target)
 
     noun_id = class_id_table["名詞"]
